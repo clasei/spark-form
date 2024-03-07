431850848035d01cf1d5adc7efcb51e677111422
clasei <clasei@proton.me>
2024-03-07 21:45:48 +0100

    firebase.js created

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRjcVv-RJf1DaCz2-qt_OSqOqFhy2_yAQ",
+  authDomain: "spark-form-7.firebaseapp.com",
+  projectId: "spark-form-7",
+  storageBucket: "spark-form-7.appspot.com",
+  messagingSenderId: "836351357849",
+  appId: "1:836351357849:web:c30d9b73c1d0f2c505d908"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
