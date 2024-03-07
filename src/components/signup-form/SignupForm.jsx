b16b91463b4b82e8915f14ef3f5c83f348488333
clasei <clasei@proton.me>
2024-03-07 18:44:03 +0100

    signup form initial setup

---
+++ src/components/signup-form/SignupForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './SignupForm.css';
+
+const SignupForm = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(email);
+  };
+
+  return (
+    <form className="signup-form" onSubmit={handleSubmit}>
+      <p className="signup-text">Do bees dream of electric bikes? Let's find out together</p>
+      <input 
+        type="email" 
+        className="signup-input" 
+        placeholder="Write here your best email" 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required 
+      />
+
+      <div>
+        <button type="submit" className="signup-button">I'm all in</button>
+      </div>
+      
+    </form>
+  );
+};
+
+export default SignupForm;
