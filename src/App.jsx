ade71bc8b25700cc84be87985cb54ebcd5641347
clasei <clasei@proton.me>
2024-03-07 20:41:43 +0100

    footer + gh link style improved

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 import Background from './components/background/Background'
 import Header from './components/header/Header'
 import SignupForm from './components/signup-form/SignupForm'
-import CodeLink from './components/CodeLink/CodeLink'
-import Footer from './components/Footer/Footer'
+import CodeLink from './components/codeLink/CodeLink'
+import Footer from './components/footer/Footer'
 
 function App() {
 
@@ -15,6 +15,8 @@ function App() {
           <div className="content">
             <Header />
             <SignupForm />
+          </div>
+          <div className="info">
             <CodeLink />
             <Footer />
           </div>
