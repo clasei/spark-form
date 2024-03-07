8b52a2498af75f1f1c22a957a4cae611ca7c4113
clasei <clasei@proton.me>
2024-03-07 12:41:36 +0100

    animated css bg initial setup

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,7 +1,11 @@
 import './Background.css';
 
 const Background = () => {
-  return <div className="background"></div>;
+  return <div className="background">
+     <div class="wave"></div>
+     <div class="wave"></div>
+     <div class="wave"></div>
+  </div>;
 };
 
 export default Background;
